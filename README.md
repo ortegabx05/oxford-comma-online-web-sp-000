4e5df5cf3cc9695c0bc1021e5845f5cdef70eaf1
jessi ortega <ortegabx05@gmail.com>
2019-07-08 17:49:08 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Oxford Comma List Formatter
+10# Oxford Comma List Formatter
 
 ## Objectives
 
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.push[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
+
